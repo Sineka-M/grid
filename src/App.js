36eb7d3712af6ec2b7b1d0c5d78165e49f8a76af
Sineka-M <snehamahemdran@gmail.com>
2024-03-27 14:41:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css'; 
+
+const GridExample = () => {
+  return (
+    <div className="grid-container">
+      <div className="grid-item item1">1</div>
+      <div className="grid-item item2">2</div>
+      <div className="grid-item item3">3</div>
+      <div className="grid-item item4">4</div>
+      <div className="grid-item item5">5</div>
+      <div className="grid-item item6">6</div>
+      <div className="grid-item item7">7</div>
+      <div className="grid-item item8">8</div>
+      <div className="grid-item item9">9</div>
+      <div className="grid-item item10">10</div>
+      <div className="grid-item item11">11</div>
+      <div className="grid-item item12">12</div>
+    </div>
+  );
+};
+
+export default GridExample;
